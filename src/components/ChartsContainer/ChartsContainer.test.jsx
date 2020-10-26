8aa9ec1b832309bb1d6a659ddaebbb25d03e40dc
Bharath Sriraam R R <albharath.1305@gmail.com>
2020-10-26 11:02:02 +0530

    update snapshot of CustomersView and rm test of ChartsContainer

--- src/components/ChartsContainer/ChartsContainer.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-
-import ChartsContainer from "./ChartsContainer";
-
-it("renders correctly", () => {
-  expect(render(<ChartsContainer />)).toMatchSnapshot();
-});
