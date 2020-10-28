5e82a6400330d0948c32c825c7641b134d980f69
Bharath Sriraam R R <albharath.1305@gmail.com>
2020-10-28 22:22:10 +0530

    fix bugs in scrolling nav functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,149 +1,140 @@
-import React, { useState, useEffect, createRef } from "react";
+/* eslint-disable */
+import React from "react";
 import { Layout, Timeline, Typography } from "antd";
 
 import { CustomersView, ChartsContainer } from "./components";
 
 import rawData from "./fixtures/data2.json";
 import DataContext from "./contexts/DataContext";
-// import useConstructor from "./hooks/useConstructor";
 
-const items = [
-  { id: "bar-chart", label: "Bar Chart" },
-  { id: "line-chart", label: "Line Chart" },
-  { id: "customers-view", label: "Customers View" },
-];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-// eslint-disable-next-line
-function buildThresholdList() {
-  const thresholds = [];
-  const numSteps = 20;
-
-  for (let i = 1.0; i <= numSteps; i += 1) {
-    const ratio = i / numSteps;
-    thresholds.push(ratio);
-  }
-
-  thresholds.push(0);
-  return thresholds;
-}
-
-function App() {
-  const [activeItem, setActiveItem] = useState(items[0].id);
-  // const [prevRatio, setPrevRatio] = useState(0);
-
-  const tableRef = createRef();
-  const barChartRef = createRef();
-  const lineChartRef = createRef();
-
-  const refs = {
-    "bar-chart": barChartRef,
-    "line-chart": lineChartRef,
-    "customers-view": tableRef,
-  };
-
-  useEffect(() => {
-    const observerConfig = {
-      // threshold: buildThresholdList(),
-      threshold: [0.6, 0.8, 1],
+    this.state = {
+      items: [
+        {
+          id: "bar-chart",
+          label: "Bar Chart",
+        },
+        {
+          id: "line-chart",
+          label: "Line Chart",
+        },
+        {
+          id: "customers-view",
+          label: "Customers View",
+        },
+      ],
+      activeItem: {
+        id: null,
+        ratio: 0,
+      },
     };
 
-    const observer = new IntersectionObserver((entries) => {
-      let maxTarget = entries[0];
+    this.itemRefs = {};
 
-      // if (maxTarget.isIntersecting && maxTarget.intersectionRatio >= 0.6) {
-      //   setActiveItem(items[0].id);
-      // }
+    this.state.items.forEach((item) => {
+      this.itemRefs[item.id] = {
+        ref: React.createRef(),
+        id: item.id,
+        ratio: 0,
+      };
+    });
 
+    const intersectionHandler = (entries) => {
       entries.forEach((entry) => {
-        console.log(
-          `${entry.target.id} :>> `,
+        this.itemRefs[entry.target.id].ratio = entry.intersectionRatio;
+      });
 
-          entry.isIntersecting,
-          entry.intersectionRatio
-        );
+      let activeEntry = this.itemRefs[this.state.items[0].id];
 
-        if (entry.intersectionRatio > maxTarget.intersectionRatio) {
-          maxTarget = { ...entry };
+      Object.values(this.itemRefs).forEach((itemRef) => {
+        if (itemRef.ratio > activeEntry.ratio) {
+          activeEntry = itemRef;
         }
       });
 
-      console.log("maxTarget :>> ", maxTarget);
+      if (activeEntry.ratio > this.state.activeItem.ratio) {
+        this.setState({ activeItem: activeEntry });
+      }
+    };
 
-      if (maxTarget.target) setActiveItem(maxTarget?.target?.id);
+    this.observer = new IntersectionObserver(intersectionHandler, {
+      threshold: new Array(101).fill(0).map((_, i) => i * 0.01),
+    });
+  }
 
-      // if (!maxTarget.target) setActiveItem(items[0].id);
-      // else setActiveItem(maxTarget?.target?.id);
-    }, observerConfig);
+  componentDidMount() {
+    Object.values(this.itemRefs).forEach((itemRef) =>
+      this.observer.observe(itemRef.ref.current)
+    );
+  }
 
-    observer.observe(tableRef.current);
-    observer.observe(barChartRef.current);
-    observer.observe(lineChartRef.current);
+  render() {
+    const { items, activeItem } = this.state;
 
-    return () => observer.disconnect();
-  }, []);
+    return (
+      <Layout style={{ padding: "2rem" }}>
+        <Typography.Title level={1} style={{ textAlign: "center" }}>
+          RevTap Task
+        </Typography.Title>
 
-  return (
-    <Layout style={{ padding: "2rem" }}>
-      <Typography.Title level={1} style={{ textAlign: "center" }}>
-        RevTap Task
-      </Typography.Title>
+        <DataContext.Provider value={{ rawData }}>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                width: "50%",
+                display: "flex",
+                flexDirection: "column",
+                gap: "0.5rem",
+              }}
+            >
+              <ChartsContainer
+                barChartRef={this.itemRefs["bar-chart"].ref}
+                lineChartRef={this.itemRefs["line-chart"].ref}
+              />
 
-      <DataContext.Provider value={{ rawData }}>
-        <div style={{ display: "flex" }}>
-          <div
-            style={{
-              width: "50%",
-              display: "flex",
-              flexDirection: "column",
-              gap: "0.5rem",
-            }}
-            id="rComponents"
-          >
-            <ChartsContainer
-              lineChartRef={lineChartRef}
-              barChartRef={barChartRef}
-            />
+              <div>
+                <CustomersView ref={this.itemRefs["customers-view"].ref} />
+              </div>
+            </div>
 
-            <div>
-              <CustomersView ref={tableRef} />
+            <div
+              style={{
+                width: "50%",
+                top: 0,
+                right: 0,
+                position: "sticky",
+              }}
+            >
+              <Timeline style={{ top: 0, right: 0, position: "sticky" }}>
+                {items.map((item) => {
+                  const isActiveItem = item.id === activeItem.id;
+                  const color = isActiveItem ? "blue" : "black";
+
+                  return (
+                    <Timeline.Item
+                      color={isActiveItem ? "blue" : "gray"}
+                      onClick={() => {
+                        this.itemRefs[item.id].ref.current.scrollIntoView({
+                          behavior: "smooth",
+                          block: "center",
+                        });
+                      }}
+                      style={{ color, cursor: "pointer" }}
+                    >
+                      {item.label}
+                    </Timeline.Item>
+                  );
+                })}
+              </Timeline>
             </div>
           </div>
-
-          <div
-            style={{
-              width: "50%",
-              top: 0,
-              right: 0,
-              position: "sticky",
-            }}
-          >
-            <Timeline style={{ top: 0, right: 0, position: "sticky" }}>
-              {items.map((item) => {
-                const isActiveItem = item.id === activeItem;
-                const color = isActiveItem ? "blue" : "black";
-                return (
-                  <Timeline.Item
-                    color={isActiveItem ? "blue" : "gray"}
-                    onClick={() => {
-                      setActiveItem(item.id);
-
-                      refs[item.id].current.scrollIntoView({
-                        behavior: "smooth",
-                        block: "center",
-                      });
-                    }}
-                    style={{ color, cursor: "pointer" }}
-                  >
-                    {item.label}
-                  </Timeline.Item>
-                );
-              })}
-            </Timeline>
-          </div>
-        </div>
-      </DataContext.Provider>
-    </Layout>
-  );
+        </DataContext.Provider>
+      </Layout>
+    );
+  }
 }
 
 export default App;
